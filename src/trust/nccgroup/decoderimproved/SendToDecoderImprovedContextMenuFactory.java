019e88d85ace61fc547226503ae23b6d83fae08e
zehuanncc <zehuan.ncc@gmail.com>
2019-11-06 01:01:10 -0800

    Improve "Send to Decoder Improved"

--- src/trust/nccgroup/decoderimproved/SendToDecoderImprovedContextMenuFactory.java
+++ src/trust/nccgroup/decoderimproved/SendToDecoderImprovedContextMenuFactory.java
@@ -25,45 +25,87 @@ class SendToDecoderImprovedContextMenuFactory implements IContextMenuFactory {
 
     @Override
     public List<JMenuItem> createMenuItems(IContextMenuInvocation invocation) {
-        ArrayList<JMenuItem> menu = new ArrayList<>();
+        List<JMenuItem> menu = new ArrayList<>();
         byte ctx = invocation.getInvocationContext();
-        ActionListener listener;
+        boolean hasRequest = false;
+        boolean hasResponse = false;
+        byte[] requestBytes = null;
+        byte[] responseBytes = null;
 
-        if (ctx == IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_REQUEST ||
-            ctx == IContextMenuInvocation.CONTEXT_MESSAGE_VIEWER_REQUEST) {
-            IHttpRequestResponse[] requestResponses = invocation.getSelectedMessages();
-            listener = event -> {
-                int start = invocation.getSelectionBounds()[0];
-                int end = invocation.getSelectionBounds()[1];
-                if (start == end) {
-                    mainTab.receiveTextFromMenu(requestResponses[0].getRequest());
-                    highlightParentTab();
-                } else {
-                    mainTab.receiveTextFromMenu(Arrays.copyOfRange(requestResponses[0].getRequest(), start, end));
-                    highlightParentTab();
-                }
-            };
-        } else if (ctx == IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_RESPONSE ||
-            ctx == IContextMenuInvocation.CONTEXT_MESSAGE_VIEWER_RESPONSE) {
-            IHttpRequestResponse[] requestResponses = invocation.getSelectedMessages();
-            listener = event -> {
-                int start = invocation.getSelectionBounds()[0];
-                int end = invocation.getSelectionBounds()[1];
-                if (start == end) {
-                    mainTab.receiveTextFromMenu(requestResponses[0].getResponse());
-                    highlightParentTab();
-                } else {
-                    mainTab.receiveTextFromMenu(Arrays.copyOfRange(requestResponses[0].getResponse(), start, end));
-                    highlightParentTab();
-                }
-            };
-        } else {
-            listener = e -> { };
+        IHttpRequestResponse[] requestResponses = invocation.getSelectedMessages();
+        switch (ctx) {
+            // Request only
+            case IContextMenuInvocation.CONTEXT_INTRUDER_PAYLOAD_POSITIONS:
+            case IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_REQUEST:
+            case IContextMenuInvocation.CONTEXT_MESSAGE_VIEWER_REQUEST:
+                requestBytes = requestResponses[0].getRequest();
+                hasRequest = true;
+                break;
+            // Response only
+            case IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_RESPONSE:
+            case IContextMenuInvocation.CONTEXT_MESSAGE_VIEWER_RESPONSE:
+                responseBytes = requestResponses[0].getResponse();
+                hasResponse = true;
+                break;
+            // Both
+            case IContextMenuInvocation.CONTEXT_INTRUDER_ATTACK_RESULTS:
+            case IContextMenuInvocation.CONTEXT_PROXY_HISTORY:
+            case IContextMenuInvocation.CONTEXT_SEARCH_RESULTS:
+            case IContextMenuInvocation.CONTEXT_SCANNER_RESULTS:
+            case IContextMenuInvocation.CONTEXT_TARGET_SITE_MAP_TABLE:
+            case IContextMenuInvocation.CONTEXT_TARGET_SITE_MAP_TREE:
+                requestBytes = requestResponses[0].getRequest();
+                responseBytes = requestResponses[0].getResponse();
+                hasRequest = true;
+                hasResponse = true;
+                break;
+            default: // should never reach here
+                Logger.printError("Unknown invocation context: " + ctx);
+                return null;
         }
 
-        JMenuItem item = new JMenuItem("Send to Decoder Improved", null);
-        item.addActionListener(listener);
-        menu.add(item);
+        // Request or response only
+        if (hasRequest ^ hasResponse) {
+            byte[] finalTextBytes = hasRequest ? requestBytes : responseBytes;
+            JMenuItem menuItem = new JMenuItem("Send to Decoder Improved");
+            menuItem.addActionListener((e) -> {
+                int start = invocation.getSelectionBounds()[0];
+                int end = invocation.getSelectionBounds()[1];
+                if (start == end) {
+                    mainTab.receiveTextFromMenu(finalTextBytes);
+                    highlightParentTab();
+                } else {
+                    mainTab.receiveTextFromMenu(Arrays.copyOfRange(finalTextBytes, start, end));
+                    highlightParentTab();
+                }
+            });
+            menu.add(menuItem);
+        } else { // Both (from the switch above there's no case that both are false)
+            JMenu subMenu = new JMenu("Send to Decoder Improved");
+            if (requestBytes != null && requestBytes.length > 0) {
+                Logger.printOutput("Req");
+                byte[] finalRequestTextBytes = requestBytes;
+                JMenuItem requestMenuItem = new JMenuItem("Request");
+                requestMenuItem.addActionListener((e) -> {
+                    mainTab.receiveTextFromMenu(finalRequestTextBytes);
+                    highlightParentTab();
+                });
+                subMenu.add(requestMenuItem);
+            }
+            if (responseBytes != null && responseBytes.length > 0) {
+                Logger.printOutput("Resp");
+                byte[] finalResponseTextBytes = responseBytes;
+                JMenuItem responseMenuItem = new JMenuItem("Response");
+                responseMenuItem.addActionListener((e) -> {
+                    mainTab.receiveTextFromMenu(finalResponseTextBytes);
+                    highlightParentTab();
+                });
+                subMenu.add(responseMenuItem);
+            }
+            if (subMenu.getItemCount() > 0) {
+                menu.add(subMenu);
+            }
+        }
         return menu;
     }
 

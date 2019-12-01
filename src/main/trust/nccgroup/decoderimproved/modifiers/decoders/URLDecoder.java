02d9478422b6411413cc4ba4521e3dbd24cc4f97
zehuanncc <zehuan.ncc@gmail.com>
2019-11-30 20:06:52 -0800

    '+' is decoded as a space character in URL decode.

--- src/main/trust/nccgroup/decoderimproved/modifiers/decoders/URLDecoder.java
+++ src/main/trust/nccgroup/decoderimproved/modifiers/decoders/URLDecoder.java
@@ -20,24 +20,20 @@ public class URLDecoder implements ByteModifier {
     public byte[] modifyBytes(byte[] input) {
         ArrayList<Byte> output = new ArrayList<>();
         for (int i = 0; i < input.length; i++ ) {
-            // If the loop is within the last two characters it can't be a url encoded character
-            if (i >= input.length-2) {
-                output.add(input[i]);
-            } else {
-                // url encoded chars start with a %
-                if (input[i] == '%') {
-                    // If the next two chars aren't valid hex chars, it isn't url encoded
-                    if (Utils.isHexDigit((char) input[i + 1]) && Utils.isHexDigit((char) input[i + 2])) {
-                        // Check if the next two chars after the % are digits
-                        // Take in the next two bytes
-                        output.add((byte) (Integer.parseInt(new String(Arrays.copyOfRange(input, i + 1, i + 3)), 16) & 0xFF));
-                        // Need to skip over the next two characters that were just decoded
-                        i += 2;
-                    }
-                } else {
-                    // Add the value of the char
-                    output.add(input[i]);
+            // '+' will be decoded as a space character, as per https://www.w3.org/TR/html4/interact/forms.html#h-17.13.4.1
+            if(input[i] == '+') {
+                output.add((byte) ' ');
+            } else if (i < input.length - 2 && input[i] == '%') {
+                // If the next two chars aren't valid hex chars, it isn't url encoded
+                if (Utils.isHexDigit((char) input[i + 1]) && Utils.isHexDigit((char) input[i + 2])) {
+                    // Check if the next two chars after the % are digits
+                    // Take in the next two bytes
+                    output.add((byte) (Integer.parseInt(new String(Arrays.copyOfRange(input, i + 1, i + 3)), 16) & 0xFF));
+                    // Need to skip over the next two characters that were just decoded
+                    i += 2;
                 }
+            } else {
+                output.add(input[i]);
             }
         }
         return Utils.convertByteArrayListToByteArray(output);
